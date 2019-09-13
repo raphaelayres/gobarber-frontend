4387ac9d9611e2eca18e17c729cf1924c63488a6
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-09-13 11:41:39 -0300

    area interna, ajustes, header e notifications

---
+++ src/components/Header/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import logo from "~/assets/logo2.svg";
+
+import { Container, Content, Profile } from "./styles";
+import Notifications from "../Notifications";
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Raphael de Falco Ayres</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="avatar"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
