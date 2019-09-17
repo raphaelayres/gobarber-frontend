d55954383cdf7d01776f1c2b56c42b800e74b103
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-09-17 12:20:28 -0300

    ajustes, melhorias e parte de perfil quase 100%

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,43 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Form, Input } from "@rocketseat/unform";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
+import { updateProfileRequest } from "~/store/modules/user/actions";
+import AvatarInput from "./AvatarInput";
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  const dispatch = useDispatch();
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar" />
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu endereço e-mail" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Sua senha atual"
+        />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,29 @@
-import { all } from "redux-saga/effects";
+import { all, takeLatest, call, put } from "redux-saga/effects";
+import { toast } from "react-toastify";
+import api from "~/services/api";
+import { updateProfileFailer, updateProfileSuccess } from "./actions";
 
-export default all([]);
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar,
+      ...(rest.oldPassword ? rest : {})
+    };
+    const response = yield call(api.put, "users", profile);
+
+    toast.success("Perfil atualizado com sucesso!");
+
+    console.log("aki", response.data);
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (error) {
+    yield put(updateProfileFailer());
+    toast.error("Erro ao atualizar perfil, confira seus dados!");
+  }
+}
+
+export default all([takeLatest("@user/UPDATE_PROFILE_REQUEST", updateProfile)]);
