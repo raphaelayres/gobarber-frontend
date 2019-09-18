415e23318cf47ede142953e42d4de8398e7bea7b
Raphael de Falco Ayres <raphaeldefalcoayres@gmail.com>
2019-09-18 11:27:35 -0300

    finalizado o módulo gobarber web

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,92 @@
-import React from "react";
+import React, { useState, useMemo, useEffect } from "react";
+import { utcToZonedTime } from "date-fns-tz";
+import {
+  format,
+  subDays,
+  addDays,
+  setHours,
+  setMinutes,
+  setSeconds,
+  isBefore,
+  isEqual,
+  parseISO
+} from "date-fns";
+import pt from "date-fns/locale/pt";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import api from "~/services/api";
-// import { Container } from './styles';
+import { Container, Time } from "./styles";
+
+const range = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
 export default function Dashboard() {
-  api.get("appointments");
-  return <h1>Dashboard</h1>;
+  const [date, setDate] = useState(new Date());
+  const [schelude, setSchelude] = useState([]);
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date]
+  );
+
+  useEffect(() => {
+    async function loadSchelude() {
+      const response = await api.get("schelude", {
+        params: { date }
+      });
+
+      const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+      const data = range.map(hour => {
+        const checkDate = setSeconds(setMinutes(setHours(date, hour), 0), 0);
+        const compareDate = utcToZonedTime(checkDate, timezone);
+
+        return {
+          time: `${hour}:00h`,
+          past: isBefore(compareDate, new Date()),
+          appointment: response.data.find(a =>
+            isEqual(parseISO(a.date), compareDate)
+          )
+        };
+      });
+
+      setSchelude(data);
+    }
+    loadSchelude();
+  }, [date]);
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
+  return (
+    <Container>
+      <header>
+        <button type="button" onClick={() => handlePrevDay()}>
+          <MdChevronLeft size={36} color="#fff" />
+        </button>
+        <strong>{dateFormatted}</strong>
+        <button type="button" onClick={() => handleNextDay()}>
+          <MdChevronRight size={36} color="#fff" />
+        </button>
+      </header>
+
+      <ul>
+        {schelude.map(time => (
+          <Time
+            past
+            key={time.time}
+            past={time.past}
+            available={!time.appointment}
+          >
+            <strong>{time.time}</strong>
+            <span>
+              {time.appointment ? time.appointment.user.name : "Em aberto"}
+            </span>
+          </Time>
+        ))}
+      </ul>
+    </Container>
+  );
 }
